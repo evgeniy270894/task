6abbbdc9395f6324a4644c819bb6d8a8bef113fa
Галицын Евгений Сергеевич <ysgalits@mts.ru>
2019-04-15 23:30:05 +0300

    написал модуль для тестового задания

---
+++ module.js
@@ -0,0 +1,50 @@
+class messageModule {
+    constructor(timer, count) {
+        this.timer = timer;
+        this.count = count;
+        this._flagTime = false;
+        this.store = [];
+    }
+
+    isQuantityOver() {
+        return !!(this.store.length > this.count)
+    }
+
+    createTimer() {
+        if (!this._flagTime) {
+            setTimeout(() => this.sendMessage('TIMER'), this.timer)
+        }
+    }
+
+    sendMessage(place) {
+        this.store = []
+        this._flagTime = false;
+        console.log(`======= Сообщения удалены из ${place}========>`, this.store)
+    }
+
+    pushMessage(message) {
+        console.log('======= Сообщение получено ! ========>', message);
+        if (!this.store.length) this.createTimer();
+        if (this.isQuantityOver()) {
+            this.sendMessage('COUNT');
+            return
+        }
+
+        this.store = [...this.store, message]
+    }
+
+}
+
+
+const timer = 10 * 1000;
+const count = 5;
+const testMessage = () => ({
+    id: Math.random(),
+    title: `Тестовое Сообщение`,
+    text: 'Привет МИР!',
+});
+
+const testModule = new messageModule(timer, count, testMessage);
+
+setInterval(() => testModule.pushMessage(testMessage()), 1000);
+
